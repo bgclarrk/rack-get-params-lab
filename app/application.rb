c765f3ab17a85867e49bda8f457af153da428370
+ <+>
2021-04-19 20:20:30 -0700

    basic shopping cart using get

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,41 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
+      
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each {|item| resp.write "#{item}\n"}
+      end
+
+    elsif req.path.match(/add/)
+      requested_item = req.params["item"]
+
+      if @@items.include?(requested_item)
+        @@cart << requested_item
+        resp.write "added #{requested_item}" 
+      else
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
@@ -21,10 +44,12 @@ class Application
   end
 
   def handle_search(search_term)
+
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
     end
+
   end
 end
